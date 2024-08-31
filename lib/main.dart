c6951cd05a79a87da3df09a755e1a787d1ee80f2
Iago F. <n3rdydzn@hotmail.com>
2024-08-31 10:45:23 -0300

    fix: better UI with more elements

--- lib/main.dart
+++ lib/main.dart
@@ -7,15 +7,27 @@ void main() {
 class MyApp extends StatelessWidget {
   MyApp({super.key});
 
-  final txtCidade = TextEditingController();
-
-  void verifyWeather() {
-    print('Verificando clima...');
-    print(txtCidade.text);
-  }
-
   @override
   Widget build(BuildContext context) {
+    final txtCidade = TextEditingController();
+
+    String temperature = '0 °C';
+    String humidity = '0%';
+    String wind = '0 KM/H';
+
+    void verifyWeather() {
+      if (txtCidade.text.isEmpty) {
+        print('Cidade não informada');
+        return;
+      }
+      print('Verificando clima...');
+      print(txtCidade.text);
+    }
+
+    void getLocation() {
+      print('Obtendo localização do dispositivo...');
+    }
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -25,59 +37,91 @@ class MyApp extends StatelessWidget {
           children: [
             Column(
               children: [
-                const Text('Digite a cidade:'),
-                TextField(
-                  controller: txtCidade,
-                  decoration:
-                      const InputDecoration(labelText: 'Digite a cidade'),
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  child: TextField(
+                    controller: txtCidade,
+                    decoration:
+                        const InputDecoration(labelText: 'Digite a cidade'),
+                  ),
                 ),
-                ElevatedButton(
-                    onPressed: () => verifyWeather(),
-                    child: const Text('Buscar'))
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                        onPressed: () => verifyWeather(),
+                        child: const Text('Buscar')),
+                    ElevatedButton(
+                        onPressed: () => getLocation(),
+                        child: const Text('Localização'))
+                  ],
+                )
               ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 Container(
+                  width: 120,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.grey),
                   padding: const EdgeInsets.all(10),
-                  color: Colors.grey,
-                  child: const Column(
+                  child: Column(
                     children: [
-                      Text(
+                      const Icon(Icons.thermostat, color: Colors.white),
+                      const Text(
                         'Temperatura:',
                         style: TextStyle(
                             color: Colors.white, fontWeight: FontWeight.bold),
                       ),
-                      Text('0°C'),
+                      Text(
+                        temperature,
+                        style: const TextStyle(color: Colors.white),
+                      ),
                     ],
                   ),
                 ),
                 Container(
+                  width: 120,
                   padding: const EdgeInsets.all(10),
-                  color: Colors.grey,
-                  child: const Column(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.grey),
+                  child: Column(
                     children: [
-                      Text(
+                      const Icon(Icons.water, color: Colors.white),
+                      const Text(
                         'Umidade:',
                         style: TextStyle(
                             color: Colors.white, fontWeight: FontWeight.bold),
                       ),
-                      Text('0%'),
+                      Text(
+                        humidity,
+                        style: const TextStyle(color: Colors.white),
+                      ),
                     ],
                   ),
                 ),
                 Container(
+                  width: 120,
                   padding: const EdgeInsets.all(10),
-                  color: Colors.grey,
-                  child: const Column(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.grey),
+                  child: Column(
                     children: [
-                      Text(
+                      const Icon(Icons.cloud, color: Colors.white),
+                      const Text(
                         'Vento:',
                         style: TextStyle(
                             color: Colors.white, fontWeight: FontWeight.bold),
                       ),
-                      Text('0 Km/h'),
+                      Text(
+                        wind,
+                        style: const TextStyle(color: Colors.white),
+                      ),
                     ],
                   ),
                 ),
